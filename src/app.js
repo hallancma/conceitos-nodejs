053b87cda533cfaeb656b66051fb94db1d1c1a9e
Hallan Christian Meirelles Alencar <hallan@MacBook-Pro-de-Hallan.local>
2020-07-20 23:11:20 -0300

    Atualizando projetos

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const { uuid, isUuid } = require("uuidv4");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:'http://localhost:8080'
+}));
 
 function validateProjectId(request,response,next){
   const { id } = request.params;
